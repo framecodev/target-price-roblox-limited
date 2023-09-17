295ea79af91c72d0491315dda244fb2cb0de0916
Leffy <122673380+framecodev@users.noreply.github.com>
2023-09-17 18:01:21 +0700

    itemv2.py

--- itemv2.py
+++ itemv2.py
@@ -31,7 +31,14 @@ def check_item_price(item_id, target_price):
 
 # item u want to buy
 # example here
-item_id_1 = 12345678 # set the item id taht you want to purchase
-target_price_1 = 1000 # set the target price example item price is 1500 and the target 1000 then set it to 1000.
-# note: Don't set the price too low
+item_id_1 = 12345678 # set the item id that you want to purchase
+target_price_1 = 1000 # set the target price example: item resale price is 1500 and your target is 1000 then set it to 1000.
+# note: Don't set the price too low, it might be no one will resale that much or mybe or take a long time
 check_item_price(item_id_1, target_price_1)
+
+# You can add more items as above like this
+item_id_2 = 12345678
+target_price_2 = 1000
+check_item_price(item_id_2, target_price_2)
+# and so on
+# note: The variable "item_id" cannot be the same, nor can "target_price" just add "_3, _4, _5" at the end of the word
